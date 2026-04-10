233caf95cfe873ef35cfe6461b7426e988d7a212
fabricioC-U <fabriciocalerourey@gmail.com>
2026-04-10 12:03:32 -0400

    FormEditar

---
+++ FormEditar.php
@@ -0,0 +1,39 @@
+<?php
+    $direccion="localhost";
+    $usuario="root";
+    $contraseña="";
+    $nombreBase="RegistrarTareas";
+    $conexion=new mysqli($direccion,$usuario,$contraseña,$nombreBase);
+    if($conexion ->error){
+        echo "Hubo un error al conectar la base de datos";
+    }
+
+    $id=$_GET['id'];
+    $sql="SELECT*FROM tareas WHERE id='$id'";
+    $resultado=$conexion->query($sql);
+    if($resultado->num_rows>0){
+        while($fila=$resultado->fetch_assoc()){
+            echo $fila['id']."<br>".$fila['nombre']."<br>". $fila['descripcion']."<br>".$fila['estado'];
+        }
+    }
+?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <form  class="caja" method="post">
+        <h1>REGISTRO DE TAREAS</h1>
+        <label for="">Nombre:</label>
+        <input type="text" name="nombre" value=<?=$nombre?>><br>
+        <label for="">Descripcion:</label>
+        <input type="text" name="descripcion" value=<?=$descripcion?>><br>
+        <label for="">Estado:</label>
+        <input type="text" name="estado" value=<?=$estado?>><br>
+        <input type="submit" value="registrar"><br>
+    </form>
+    </body>
+    </html>
